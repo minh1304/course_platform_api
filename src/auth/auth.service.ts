1c2ba02a9c06a3a0b3b018af3068030331a3d8be
Tuan Minh <vtm1304@gmail.com>
2025-03-21 13:42:46 +0700

    Init

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class AuthService {
+    constructor(private prisma: PrismaService) {
+    }
+    login() {
+
+    }
+
+    signup() {
+        return {msg: "I am sign up"};
+    }
+}
