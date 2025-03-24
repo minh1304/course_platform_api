e3db2e767e058de7e27d883066b08291e7473356
Tuan Minh <vtm1304@gmail.com>
2025-03-24 13:41:10 +0700

    Minh Vo: Update Login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,74 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {
-    }
-    login() {
+    constructor(private prisma: PrismaService) {}
 
-    }
+    async signup(dto: AuthDto) {
+        try {
+            // generate the password hash
+            const hash = await argon.hash(dto.password);
 
-    signup() {
-        return {msg: "I am sign up"};
+            const user = await this.prisma.user.create({
+              data: {
+                email: dto.email,
+                hash: hash,
+                firstName: "Minh",
+                lastName: "Vo"
+              },
+              select: {
+                id: true,
+                email: true,
+                createdAt: true
+              }
+            });
+      
+            return user;
+          } catch (error) {
+            if (
+              error instanceof
+              PrismaClientKnownRequestError
+            ) {
+              if (error.code === 'P2002') {
+                throw new ForbiddenException(
+                  'Credentials taken',
+                );
+              }
+            }
+            throw error;
+          }
+    }
+    async signin(dto: AuthDto) {
+      try {
+        const user = await this.prisma.user.findUnique({
+          where: {
+            email: dto.email,
+          },
+        })
+        
+        if(!user) throw new ForbiddenException('Credentials Incorrect!');
+        
+        // compare password
+        const pwMatches = await argon.verify(user.hash, dto.password);
+        
+        if(!pwMatches) throw new ForbiddenException('Credentials Incorrect!');
+
+        const { hash, ...userWithoutHash } = user;
+
+        return userWithoutHash;
+
+      }
+      catch (error) {
+        if (error instanceof PrismaClientKnownRequestError) {
+          if (error.code === 'P2002') {
+            throw new ForbiddenException('Credentials taken');
+          }
+        }
+        throw error;
+      }
     }
 }
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,20 +1,9 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { PrismaClient } from '@prisma/client';
 
 @Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService]
 })
-export class PrismaModule extends PrismaClient {
-  constructor() {
-    super({
-      datasources: {
-        db: {
-          url: 'postgresql://postgres:123@localhost:5434/nest?schema=public'
-        }
-      }
-    })
-  }
-}
+export class PrismaModule{}
