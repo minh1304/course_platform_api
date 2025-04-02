79f8c992dbb5f1364d30ea89bc79f700da49b8e6
Tuan Minh <vtm1304@gmail.com>
2025-04-02 21:35:16 +0700

    Minh Vo: create and get course

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common';
+import { Course } from '@prisma/client';
+import { CreateCourseDto } from 'src/auth/dto/createCourse.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class CoursesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllCourses(): Promise<Course[]> {
+    return await this.prisma.course.findMany({
+      include: {
+        sections: {
+          include: {
+            chapters: true,
+          },
+        },
+        enrollments: {},
+      },
+    });
+  }
+  async createCourse(data: CreateCourseDto) {
+    return await this.prisma.course.create({
+      data: {
+        title: data.title,
+        description: data.description,
+        category: data.category,
+        image: data.image,
+        price: data.price,
+        level: data.level,
+        status: data.status,
+        teacherId: data.teacherId,
+        teacherName: data.teacherName,
+        sections: {
+          create: data.sections.map((section) => ({
+            sectionTitle: section.sectionTitle,
+            sectionDescription: section.sectionDescription,
+            chapters: {
+              create: section.chapters.map((chapter) => ({
+                type: chapter.type,
+                title: chapter.title,
+                content: chapter.content,
+              })),
+            },
+          })),
+        },
+        enrollments: {
+            create: data.enrollments?.map(enrollment => ({
+                userId: enrollment.userId
+            })) || []
+        }
+      },
+      include: {
+        sections: {
+          include: {
+            chapters: true,
+          },
+        },
+        enrollments: true,
+      },
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
+    whitelist: false,
   }))
   app.enableCors({
     origin: 'http://localhost:3000', // Allow frontend URL
